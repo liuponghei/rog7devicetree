9f9436e4240389d19cca862a10ddd2df20fb5b9d
liuponghei <liuponghei@users.noreply.github.com>
2024-07-20 06:44:44 +0800

    Update and rename lineage_ASUS_AI2205.mk to ASUS_AI2205.mk

--- ASUS_AI2205.mk
+++ ASUS_AI2205.mk
@@ -8,21 +8,25 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
 
 # Inherit from ASUS_AI2205 device
 $(call inherit-product, device/asus/ASUS_AI2205/device.mk)
 
 PRODUCT_DEVICE := ASUS_AI2205
-PRODUCT_NAME := lineage_ASUS_AI2205
+PRODUCT_NAME := ASUS_AI2205
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_AI2205
 PRODUCT_MANUFACTURER := asus
 
+PRODUCT_SYSTEM_NAME := $(PRODUCT_MODEL)
+PRODUCT_SYSTEM_DEVICE := ASUS_AI2205
+
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="WW_AI2205-user 14 UKQ1.230917.001 34.1010.0820.85-0 release-keys"
+    PRIVATE_BUILD_DESC="WW_AI2205-user 14 UKQ1.230917.001 34.1010.0820.85-0 release-keys"  \
+    TARGET_DEVICE=$(PRODUCT_SYSTEM_DEVICE) \
+    TARGET_PRODUCT=$(PRODUCT_SYSTEM_NAME)
 
 BUILD_FINGERPRINT := asus/WW_AI2205/ASUS_AI2205:14/UKQ1.230917.001/34.1010.0820.85-0:user/release-keys
